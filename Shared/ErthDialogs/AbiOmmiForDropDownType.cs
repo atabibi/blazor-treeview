d3952bac4ef42fb35fbab91f020bdf38c04fa94d
Abbas Tabibi <tabibi.abbas@gmail.com>
2020-09-22 22:09:31 +0330

    قبل از افزودن امکان ذخیره پرونده مورث

---
+++ Shared/ErthDialogs/AbiOmmiForDropDownType.cs
@@ -0,0 +1,9 @@
+using T00.Shared.ErthTreeCodes;
+
+namespace T00.Shared.ErthDialogs
+{    
+    public class AbiOmmiForDropDownType {
+        public AbiOmmi AbiOmmi { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Shared/ErthDialogs/GenderForDropDownType.cs
@@ -0,0 +1,10 @@
+using T00.Shared.ErthTreeCodes;
+
+namespace T00.Shared.ErthDialogs
+{
+     public class GenderForDropDownType {
+        public Gender Gender { get; set; }
+        public string Title { get; set; }
+    }
+
+}
